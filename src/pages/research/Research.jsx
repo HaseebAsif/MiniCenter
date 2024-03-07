9bc3a1f4e03c177ecf95a9f735c0604a07fbf16e
asfarwaheed01 <asfar1024962@gmail.com>
2024-03-07 21:22:33 +0500

    research page lines

--- src/pages/research/Research.jsx
+++ src/pages/research/Research.jsx
@@ -1,5 +1,4 @@
 import Container from "../../common/Container";
-import React from "react";
 import Nav from "../../components/Navbar/Navbar";
 import graph from "../../assets/images/Research PSD Images/graph.png";
 import sec2 from "../../assets/images/Research PSD Images/r-sec-2.png";
@@ -17,7 +16,7 @@ const Research = () => {
       <section className="bg-[#0a2159] min-h-screen">
         <Container>
           <Nav />
-          <section className="r-sec-1 h-full md:pt-[10%] pt-[25%] text-white">
+          <section className="r-sec-1 h-full md:pt-[10%] pt-[25%] text-white relative z-[2]">
             <h1 className="md:text-[60px] text-[30px] font-semibold text-center">
               Research
             </h1>
@@ -29,22 +28,22 @@ const Research = () => {
               dictum, mi ut aliquam porta, dui enim eleif end ante, sit amet
               convallis metus ligula quis purus.
             </p>
-            <div className="img-sec-1 md:flex md:py-10 py-2  items-center">
+            <div className="img-sec-1 md:flex md:py-[5%] py-2  items-center">
               <div className="image md:w-[50%] w-[90%] mx-auto">
                 <img src={graph} alt="" />
               </div>
-              <div className="r-sec-1-text md:text-left text-center md:w-[50%] w-[90%] mx-auto">
+              <div className="r-sec-1-text md:text-left text-center md:w-[50%] w-[90%] mx-auto relative z-[2]">
                 <h2 className="md:text-[36px] text-[24px] pt-3 md:pt-0 font-semibold">
                   NASA Shadow Sheild Genetic Algorithm
                 </h2>
                 <p className="md:text-[24px] text-[16px] md:py-8 py-4 leading-[1.5] md:pr-8">
-                  Steven conducted research into optimizing NASA's Shadow
+                  Steven conducted research into optimizing NASA&apos;s Shadow
                   Shield, utilizing C++ to develop his Genetic Algorithm which
                   increased efficiency of determining the optimal dimensions and
                   weight of the shield. Shield performance through simulation of
                   radiation collision was output onto a readable graph by
-                  Steven's work. The algorithm produced results in a fraction of
-                  the time it would have taken humans.
+                  Steven&apos;s work. The algorithm produced results in a
+                  fraction of the time it would have taken humans.
                 </p>
               </div>
             </div>
@@ -59,16 +58,17 @@ const Research = () => {
                 <p className="md:text-[24px] text-[16px] md:py-8 py-4 leading-[1.5]">
                   Arvind assisted in the training and optimization of a You Only
                   Look Once (YOLO) real-time image detection neural network as
-                  part of a research project in Clemson University's Department
-                  of Mechanical Engineering. This involved using the YOLO neural
-                  network to analyze athletes faces at different stages of
-                  exercise intensity, associate with it a level of fatigue, and
-                  provide feedback. He also created and tested polynomial-time
-                  convex optimization algorithms for YOLO with the Fast R-CNN
-                  method to decrease background mistakes and increase accuracy
-                  in embedded computer vision systems. This has given Arvind
-                  proficiency in the model training process, allowing him to
-                  adapt real-time image detection for various objectives.
+                  part of a research project in Clemson University&apos;s
+                  Department of Mechanical Engineering. This involved using the
+                  YOLO neural network to analyze athletes faces at different
+                  stages of exercise intensity, associate with it a level of
+                  fatigue, and provide feedback. He also created and tested
+                  polynomial-time convex optimization algorithms for YOLO with
+                  the Fast R-CNN method to decrease background mistakes and
+                  increase accuracy in embedded computer vision systems. This
+                  has given Arvind proficiency in the model training process,
+                  allowing him to adapt real-time image detection for various
+                  objectives.
                 </p>
               </div>
             </div>
@@ -77,7 +77,7 @@ const Research = () => {
       </section>
       <section className="bg-[#eaeefa] min-h-screen">
         <Container>
-          <section className="r-sec-2 pt-20 text-black">
+          <section className="r-sec-2 pt-20 text-black relative z-[2]">
             <h1 className="md:text-[60px] text-[36px] font-semibold text-center">
               Our Upcoming Projects
             </h1>
@@ -155,15 +155,11 @@ const Research = () => {
           </section>
         </Container>
       </section>
-      <img
-        src={rline}
-        alt=""
-        className="absolute top-0 right-0 h-full z-[-1]"
-      />
+      <img src={rline} alt="" className="absolute top-0 right-0 h-full z-[0]" />
       <img
         src={bline}
         alt=""
-        className="absolute left-[12%] bottom-0 h-full z-[-1]"
+        className="absolute left-[12%] bottom-0 h-full z-[0]"
       />
       <Footer />
     </section>
